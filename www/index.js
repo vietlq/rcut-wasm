46dd83579b1dba322b36925a36354d9e0ff62787
Viet Le <vietlq85@gmail.com>
2020-04-18 17:13:03 +0100

    merged bytes

--- www/index.js
+++ www/index.js
@@ -11,8 +11,13 @@ function ready() {
     console.log(merged_result);
 
     let unmerged_bytes = wasm.rcut_bytes(birds, char_ranges);
+    let merged_bytes = wasm.rcut_bytes(birds, char_ranges);
 
-    document.getElementById("content").innerText = unmerged_result + merged_result + unmerged_bytes;
+    document.getElementById("content").innerText = [
+        unmerged_result,
+        merged_result,
+        unmerged_bytes,
+        merged_bytes].join('');
 }
 
 //document.addEventListener("DOMContentLoaded", ready);
